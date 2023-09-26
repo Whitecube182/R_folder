0b0c9c00affd52b91886f8d28f113a29d4fb2e05
Whitecube182 <evenoscarharlert@outlook.com>
2023-09-26 17:46:18 +0200

    Update
    Just todays work finished

--- Weekly projects/Quiz 1/quiz.R
+++ Weekly projects/Quiz 1/quiz.R
@@ -1,7 +1,19 @@
 #Load lib ----
 library(tidyverse)
+install.packages("ggplot")
+install.packages("ggrepel")
+install.packages("tidyverse")
+install.packages(
+  c("arrow", "babynames", "curl", "duckdb", "gapminder", 
+    "ggrepel", "ggridges", "ggthemes", "hexbin", "janitor", "Lahman", 
+    "leaflet", "maps", "nycflights13", "openxlsx", "palmerpenguins", 
+    "repurrrsive", "tidymodels", "writexl")
+)
 
-
+library(tidyverse)
+library(ggrepel)
+library(nycflights13)
+library()
 
 #Code ----
 
