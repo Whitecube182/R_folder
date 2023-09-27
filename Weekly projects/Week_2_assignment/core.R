4b811dbe7277a0b438bcdd41f56db0d48216563d
Whitecube182 <106096421+Whitecube182@users.noreply.github.com>
2023-09-27 15:39:26 +0200

    Test commit

--- Weekly projects/Week_2_assignment/core.R
+++ Weekly projects/Week_2_assignment/core.R
@@ -2,40 +2,62 @@
 
 library(nycflights13)
 library(tidyverse)
+library(palmerpenguins)
+library(ggthemes)
 
 
 #Code starts here ----
 
-view(nycflights13::flights)
-summary(nycflights13::flights)
+view(palmerpenguins::penguins_raw)
 
-filter(nycflights13::flights, arr_time >= 1800)
-
-
-#We are looking for 6pm (1800) until 1159pm (23:59)
-filter(nycflights13::flights, between(arr_time, 1800, 2359))
-
-# select(), contains(), 
-
-select(nycflights13::flights, contains("dep_delay"))
-
-# Find all flights that made up at least 10 minutes of delay
-# at departure during the flight
+glimpse(palmerpenguins::penguins)
 
 
-
-
-#By default, missing values are placed at the end of the 
-#the resulting tibble. How could you use arrange() to sort
-#all the missing values to be at the start?
-
-arrange(nycflights13::flights, desc(is.na(dep_time)))
-
-
-
-
-flights_times <- mutate(nycflights13::flights,
-  dep_time_mins = (dep_time %/% 100 * 60 + dep_time %% 100) %% 1440,
-  sched_dep_time_mins = (sched_dep_time %/% 100 * 60 +
-  sched_dep_time %% 100) %% 1440
+#First two frames, defines y and x axel, miss the fram on top
+ggplot(
+  data = penguins,
+  mapping = aes(x = flipper_length_mm, y = body_mass_g)
 )
+
+#Geom introduced, third layer placed with the relations on length and bodymass
+
+ggplot (
+  data = penguins,
+  mapping = aes(x = flipper_length_mm, y = body_mass_g)
+) +
+  geom_point()
+
+#Same as above but added colors to the different species,
+#NB! Max 6 colors available?! I think
+
+ggplot(
+  data = penguins,
+  mapping = aes(x = flipper_length_mm, y = body_mass_g, color = species)
+) +
+  geom_point()
+
+#Same as above, added an extra layer with the smooth lines
+#Unsure about the argument "lm" and what it actually does
+
+ggplot(
+  data = penguins,
+  mapping = aes(x = flipper_length_mm, y = body_mass_g, color = species)
+) +
+  geom_point() +
+  geom_smooth(method = "lm")
+
+
+
+#Next step we remove the color from aes and move it to geom point
+#This is to assure that we give it one long line instead of adding 
+#One for each species
+
+ggplot(
+  data = penguins,
+  mapping = aes(x = flipper_length_mm, y = body_mass_g)
+) +
+  geom_point(mapping = aes(color = species, shape = species)) +
+  geom_smooth(method = "lm")
+
+#Might have been mistaken before, might be 6 shapes instaed of colors
+
